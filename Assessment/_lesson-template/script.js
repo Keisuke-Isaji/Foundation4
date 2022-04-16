b5bd23994204642b068db2dae6937d4822385c08
Keisuke-Isaji <keisuke_isaji@mail.toyota.co.jp>
2022-04-16 09:34:12 +0900

    3

--- Assessment/_lesson-template/script.js
+++ Assessment/_lesson-template/script.js
@@ -54,3 +54,59 @@ function findKeys(obj, target) {
 test(findKeys({ a: 1, b: 2, c: 6, d: 4, e: 2 }, 2), ["b", "e"]);
 test(findKeys({ 1: "h", b: "el", c: "hello", d: "hello", e: "o" }, "hello"), ["c", "d"]);
 
+// 3.buildObject という名前の関数を宣言してください。
+console.log("3.buildObject という名前の関数を宣言してください。");
+// この関数は引数として 2 つの配列を受け取り、新しいオブジェクトを返します。
+// リターンするオブジェクトは、第一引数の配列の要素をオブジェクトのキーとして設定し、第二引数の配列の要素をオブジェクトの値として設定します。
+
+function buildObject(array1, array2) {
+    const result = {};
+    for (let i = 0; i < array1.length; i++) {
+        result[array1[i]] = array2[i];
+    }
+    return result;
+}
+
+test(buildObject(["a", "b", "c"], [1, 2, 3]), {"a": 1, "b": 2, "c": 3});
+test(buildObject(["cat", "dog", "duck"], ["meow", "woof", "quack"]), {"cat": "meow", "dog": "woof", "duck": "quack"});
+test(buildObject(["cat", "dog", "duck"], [null, 0, NaN]), {"cat": null, "dog": 0, "duck": NaN});
+test(buildObject(["abc", "def", "ghi"], [[0, 1, 2], [3, 4, 5], [6, 7, 8]]), {"abc": [0, 1, 2], "def": [3, 4, 5], "ghi": [6, 7, 8]});
+
+// 4.add という名前の関数を宣言してください。
+console.log("4.add という名前の関数を宣言してください。")
+// この関数は引数として 1 つの数値 x を受け取り、引数として 1 つの数値 y を受けとる関数を返します。
+// リターンされた関数は、実行時に ‘x’ と ‘y’ の和を返します。
+
+function add(x) {
+    return y => x + y;
+}
+
+const addTwo = add(2);
+test(addTwo(3), 5);
+test(addTwo(70), 72);
+
+const addOneHundred = add(100);
+test(addOneHundred(3), 103);
+
+// このコードを実行すると、どの順番で何が表示されるでしょうか？
+// もちろん、コードを実行する前に答えてください。なぜそうなるのか、わからない場合はわかるまで調べましょう！
+
+function simpleHello() {
+    console.log("Hello");
+}
+
+function anotherGreeting(name) {
+    return "Hello, " + name;
+}
+
+const foo = simpleHello();
+const bar = anotherGreeting("JavaScript");
+
+console.log(foo);
+console.log(bar);
+
+// あなたの回答と、なぜそうなるのかの説明をここに記載してください
+// Hello; -> console.log(foo)で、コンソールにHelloを表示する関数simpleHelloを実行。
+// undefined; ->　simpleHelloの返り値はundefined
+// Hello, JavaScript; -> console.log(bar)で"JavaScript"を引数に取り、関数anotherGreetingを実施。その返り値を表示。
+
