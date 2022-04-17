55a54366e4495ce0e5ee58660e4d6859a15a6eac
Keisuke-Isaji <keisuke_isaji@mail.toyota.co.jp>
2022-04-17 22:53:19 +0900

    1

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 'use strict';
 // please do not delete the 'use strict' line above
 
-document.getElementById('color-button').addEventListener('click', changeColor)
+document.getElementById('color-button').addEventListener('click', changeColor);
 
 function changeColor() {
+  document.body.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
   console.log('Button clicked!'); // feel free to change/delete this line
 }
